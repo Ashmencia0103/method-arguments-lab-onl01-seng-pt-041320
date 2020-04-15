2b3df47588e9ad8f3909264f576348e53f39de60
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-15 22:59:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,8 +4,3 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end 
 
-# lib/introduction_with_langauge
-
-def introduction_with_langauge("name","language")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
